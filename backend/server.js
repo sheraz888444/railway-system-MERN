f7ff5997b83d54d37790725e8ba6ed7ce91fcb3a
sheraz888444 <itssheraz78618@gmail.com>
2026-02-21 14:14:41 +0500

    updated port

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Rate limiting
 const limiter = rateLimit({
